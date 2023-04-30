f0292068962b4b662858cb3671610a80a7146a51
Eray Chumak <eray_chumak@protonmail.com>
2023-04-30 05:12:13 +0100

    Update user model

--- app/User.php
+++ app/User.php
@@ -6,34 +6,27 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
+use App\Questionnaire;
+
 class User extends Authenticatable
 {
     use Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
     protected $fillable = [
-        'name', 'email', 'password',
+        "name", "email", "password",
     ];
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
     protected $hidden = [
-        'password', 'remember_token',
+        "password", "remember_token",
     ];
 
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
     protected $casts = [
-        'email_verified_at' => 'datetime',
+        "email_verified_at" => "datetime"
     ];
+
+    // The questionnaires that belong to the user.
+    public function questionnaires()
+    {
+        return $this->hasMany(Questionnaire::class, "user_id");
+    }
 }
