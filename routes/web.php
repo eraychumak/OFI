ef6268fe3b34483e778f93ca0026beec299f11b9
Eray Chumak <eray_chumak@protonmail.com>
2023-04-30 05:16:00 +0100

    Add protected and guest routes

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,18 @@
 Auth::routes();
 
 Route::get("/", "QuestionnaireController@index");
+
+Route::get("/questionnaires/create", "QuestionnaireController@create")->middleware("auth");
+Route::post("/questionnaires/create", "QuestionnaireController@store")->middleware("auth");
+Route::get("/questionnaires/{questionnaire}", "QuestionnaireController@show");
+Route::get("/questionnaires/{questionnaire}/edit", "QuestionnaireController@edit")->middleware("auth");
+Route::put("/questionnaires/{questionnaire}/update", "QuestionnaireController@update")->middleware("auth");
+
+Route::post("/questionnaires/{questionnaire}/submit", "RespondentController@create");
+Route::get("/questionnaires/{questionnaire}/responses", "RespondentController@index")->middleware("auth");
+
+Route::get("/questionnaires/{questionnaire}/questions/create", "QuestionController@create")->middleware("auth");
+Route::post("/questionnaires/{questionnaire}/questions/create", "QuestionController@store")->middleware("auth");
+Route::get("/questionnaires/{questionnaire}/questions/{question}", "QuestionController@show")->middleware("auth");
+Route::put("/questionnaires/{questionnaire}/questions/{question}", "QuestionController@update")->middleware("auth");
+Route::delete("/questionnaires/{questionnaire}/questions/{question}", "QuestionController@destroy")->middleware("auth");
