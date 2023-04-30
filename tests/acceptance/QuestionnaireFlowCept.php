f30eb0cf9ab2ba74f34573cd2b2676fe84f64d80
Eray Chumak <eray_chumak@protonmail.com>
2023-04-30 05:10:02 +0100

    Add QuestionnaireFlow BDD

---
+++ tests/acceptance/QuestionnaireFlowCept.php
@@ -0,0 +1,23 @@
+<?php 
+$I = new AcceptanceTester($scenario);
+$I->wantTo('perform actions and see result');
+
+$I->amOnPage("/");
+$I->see("Login");
+$I->click("Login");
+$I->fillField("email", "codecept@gmail.com");
+$I->fillField("password", "password");
+$I->click("#loginBtn");
+
+$I->amOnPage("/");
+$I->see("Create a new questionnaire");
+$I->click("#createNewQuestionnaireBtn");
+
+$I->see("Create a questionnaire");
+$I->fillField("title", "Code Cept - Test");
+$I->fillField("description", "This questionnaire is for testing purposes.");
+$I->click("#createQuestionnaireBtn");
+
+$I->see("Code Cept - Test");
+$I->see("View as admin");
+$I->see("Questions");
