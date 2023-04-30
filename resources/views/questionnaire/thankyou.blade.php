6391d5993483f95d974890b6e3a1df0558bd07cb
Eray Chumak <eray_chumak@protonmail.com>
2023-05-01 00:01:57 +0100

    Re-word thank you blade

--- resources/views/questionnaire/thankyou.blade.php
+++ resources/views/questionnaire/thankyou.blade.php
@@ -3,11 +3,10 @@
 @section("main")
 <main>
   <header>
-    <p>Questionnaire</p>
+    <p class="tag">Questionnaire</p>
     <h1>{{ $questionnaire->title }}</h1>
-    <p>Description: <i>{{ $questionnaire->description }}</i></p>
   </header>
-  <p>Thank you for submitting your response to the questionnaire</p>
+  <p>Thank you for completing the questionnaire.</p>
   <a href="/">Go to the home page</a>
 </main>
 @endsection
