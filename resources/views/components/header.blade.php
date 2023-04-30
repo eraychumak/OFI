0dc5e9919b331951a96486ae6830cce1e6871546
Eray Chumak <eray_chumak@protonmail.com>
2023-04-30 05:14:53 +0100

    Add login and logout to header to pass AuthFlow BDD

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -14,4 +14,16 @@
   </div>
   @endguest
 
+  @auth
+    <p>Logged in as: {{ Auth::user()->name }}</p>
+    <a
+      href="/logout"
+      onclick="event.preventDefault(); document.getElementById('logoutForm').submit();"
+    >
+      Logout
+    </a>
+    <form action="/logout" method="POST" id="logoutForm" style="display: none;">
+      @csrf
+    </form>
+  @endauth
 </header>
